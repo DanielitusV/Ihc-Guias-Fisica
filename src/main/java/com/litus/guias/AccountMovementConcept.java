f2405b710be9a19aacf9a1513b7fb72041ccaa8b
DanielitusV <virreira.daniel@gmail.com>
2026-08-14 19:46:10 -0400

    Agregar restricciones a los datos de los movimientos

---
+++ src/main/java/com/litus/guias/AccountMovementConcept.java
@@ -0,0 +1,10 @@
+package com.litus.guias;
+
+public enum AccountMovementConcept {
+    SALE,
+    GENERAL_EXPENSE,
+    SUPPLIER_PAYMENT,
+    SALE_CANCELLATION,
+    CLOSURE_ADJUSTMENT
+}
+
---
+++ src/main/java/com/litus/guias/AccountMovementType.java
@@ -0,0 +1,6 @@
+package com.litus.guias;
+
+public enum AccountMovementType {
+    INCOME,
+    EXPENSE
+}
